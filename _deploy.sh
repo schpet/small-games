2569f00140ec1b286fad8a78c98d84387ef076ae
Peter Schilling <peter@peterschilling.org>
2013-08-12 18:42:38 +0200

    updating deploy scripts

--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 jekyll build -d dist --config _config-deploy.yml
+git add dist && git commit -m "building site for deploy"
 git subtree push --prefix dist origin gh-pages
--- dist/Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-ruby '1.9.3'
-
-gem "jekyll", "~> 1.1.2"
-gem 'coffee-script'
-gem 'jekyll-assets'
-gem 'therubyracer'
-gem 'coffee-script'
-gem 'sass'
