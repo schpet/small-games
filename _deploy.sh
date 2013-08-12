e47ccc8139e6a3284c64658611ef2a5359542f04
Peter Schilling <peter@peterschilling.org>
2013-08-12 18:49:48 +0200

    remove next link

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 jekyll build -d dist --config _config-deploy.yml
-git add dist && git commit -m "building site for deploy"
+git add dist
+git commit dist -m "Building site for gh-pages"
 git subtree push --prefix dist origin gh-pages
